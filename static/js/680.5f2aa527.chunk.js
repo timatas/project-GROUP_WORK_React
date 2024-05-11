ffac36e019367013e1abc323f7cfb6bfd29b3191
OlehKotynskyi <88795955+OlehKotynskyi@users.noreply.github.com>
2024-05-11 19:58:25 +0000

    Deploying to gh-pages from @ OlehKotynskyi/project-GROUP_WORK_React@3df3a175ea56db02419a2b342ce40d5138b3b581 🚀

--- static/js/680.5f2aa527.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[680],{680:function(e,r,n){n.r(r);var a=n(3563),c=n(184);r.default=function(){return(0,c.jsxs)("div",{className:a.Z.container,children:[(0,c.jsx)("h1",{children:"HomePage"}),(0,c.jsx)("a",{href:"/signup",children:"Try tracker"}),(0,c.jsx)("a",{href:"/signin",children:"Sign In"})]})}}}]);
-//# sourceMappingURL=680.5f2aa527.chunk.js.map
